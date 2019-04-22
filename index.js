7405530943b7ce22c51a53fdc19b0738078912b6
Jacob Senteney <jwsenteney@gmail.com>
2019-04-22 16:02:53 -0400

    Loading react app and opening window on app start

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const electron = require("electron");
 const { app, BrowserWindow } = electron;
 
-let browserWindow;
+let mainWindow;
 
 app.on("ready", () => {
-  browserWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     webPreferences: { backgroundThrottling: false }
   });
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
