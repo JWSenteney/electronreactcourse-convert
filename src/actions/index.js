df92e28b8f80bf37dc5a04dc7201909e9816c1d5
Jacob Senteney <jwsenteney@gmail.com>
2019-04-24 02:24:00 -0400

    Send convert signal to electron with video prefs and convert them

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { ipcRenderer } from "electron";
 import {
   ADD_VIDEO,
@@ -8,7 +9,7 @@ import {
   VIDEO_COMPLETE
 } from "./types";
 
-// TODO: Communicate to MainWindow process that videos
+// Communicate to MainWindow process that videos
 // have been added and are pending conversion
 export const addVideos = videos => dispatch => {
   ipcRenderer.send("videos:added", videos);
@@ -21,7 +22,10 @@ export const addVideos = videos => dispatch => {
 // to start converting videos.  Also listen for feedback
 // from the MainWindow regarding the current state of
 // conversion.
-export const convertVideos = () => (dispatch, getState) => {};
+export const convertVideos = () => (dispatch, getState) => {
+  const videos = _.map(getState().videos);
+  ipcRenderer.send("conversion:start", videos);
+};
 
 // TODO: Open the folder that the newly created video
 // exists in
